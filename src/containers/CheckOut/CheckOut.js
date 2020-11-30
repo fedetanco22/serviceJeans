6099cab78f07bbec0616efcb384f0168d8716f9e
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2020-11-29 22:51:07 -0300

    Checkout

--- src/containers/CheckOut/CheckOut.js
+++ src/containers/CheckOut/CheckOut.js
@@ -1,1 +1,9 @@
-export default function CheckOut() {}
+import "./CheckOut.scss";
+
+export default function CheckOut() {
+  return (
+    <div>
+      <h1 className="titulo">This is the check out</h1>
+    </div>
+  );
+}
