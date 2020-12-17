e2be6d1b3e314a39838ff121b55b9a3863518a58
fede tanco <63819426+fedetanco22@users.noreply.github.com>
2020-12-17 12:00:58 -0300

    CartCounter

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -2,17 +2,19 @@ import Cart from "../Cart/Cart";
 import "./CartList.scss";
 
 export default function CartList({ products }) {
-  return (
-    <div id="Cart">
-      <div className="cart__titulos container">
-        <h6 className="cart__titulos__text">Producto</h6>
-        <h6 className="cart__titulos__text">Descripción</h6>
-        <h6 className="cart__titulos__text">Cantidad</h6>
-        <h6 className="cart__titulos__text">Precio</h6>
-      </div>
-      {products.map((product, key) => (
-        <Cart key={key} product={product} />
-      ))}
-    </div>
-  );
+	return (
+		<div id='Cart'>
+			<div className='cart__titulos container'>
+				<h6 className='cart__titulos__text'>Producto</h6>
+				<h6 className='cart__titulos__text'>Descripción</h6>
+				<h6 className='cart__titulos__text'>Cantidad</h6>
+				<h6 className='cart__titulos__text'>Precio</h6>
+			</div>
+			<div>
+				{products.map((product, key) => (
+					<Cart key={key} product={product} />
+				))}
+			</div>
+		</div>
+	);
 }
